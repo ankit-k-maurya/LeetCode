051a30198f013d1031db2b83a48eecb91fc09c50
Ankit Kumar Maurya <31803636+ankit-k-maurya@users.noreply.github.com>
2026-07-20 16:02:14 +0530

    Time: 1 ms (57.93%), Space: 58.7 MB (59.52%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,10 @@
+function removeDuplicates(nums: number[]): number {
+    let index = 1;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[index-1]){
+            nums[index] = nums[i];
+            index++
+        }
+    }
+    return index;
+};
