4cf55b8eeb408e56fe11f3e471506ebfe4775571
Ankit Kumar Maurya <31803636+ankit-k-maurya@users.noreply.github.com>
2026-08-24 09:32:34 +0530

    Time: 44 ms (16.68%), Space: 70 MB (15.82%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -0,0 +1,10 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const hash: Record<string, string[]> = {};
+    
+    for (const word of strs) {
+        const key = [...word].sort().join('');
+        hash[key] ||= []; 
+        hash[key].push(word);
+    }
+    return Object.values(hash);
+}
