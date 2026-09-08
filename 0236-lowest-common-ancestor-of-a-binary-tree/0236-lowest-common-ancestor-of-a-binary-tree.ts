88b8d5def6f726368302e4965155ff35cc509376
Ankit Kumar Maurya <31803636+ankit-k-maurya@users.noreply.github.com>
2026-09-08 08:33:49 +0530

    Time: 102 ms (38.79%), Space: 88.7 MB (89.22%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	if(root === null || root === p || root === q) return root;
+    let left: TreeNode | null = lowestCommonAncestor(root.left,p,q);
+    let right: TreeNode | null = lowestCommonAncestor(root.right,p,q);
+    if(left === null) return right;
+    else if(right === null) return left;
+    else return root;
+};
